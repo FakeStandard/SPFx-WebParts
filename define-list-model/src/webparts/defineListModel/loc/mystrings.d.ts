1e561f6c2c899a847b1e044b197c187f5039d503
FakeStandard <rileylin.space@gmail.com>
2022-02-12 14:42:58 +0800

    add define list model demo

---
+++ define-list-model/src/webparts/defineListModel/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDefineListModelWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DefineListModelWebPartStrings' {
+  const strings: IDefineListModelWebPartStrings;
+  export = strings;
+}
